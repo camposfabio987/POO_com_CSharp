e082ce94c9da1d6ff9a1e893d7f4842f6a0c6903
camposfabio987 <campos.fabio987@gmail.com>
2022-07-01 08:24:12 -0300

    pasta com codigos em c#

---
+++ CSharp/meuPrimeiroProjeto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace meuPrimeiroProjeto 
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta_do_Fabio = new ContaCorrente ("Fábio Henrique", 89777, 820, 50000);
+            ContaCorrente conta_do_Renatin = new ContaCorrente ("Renatin do Celta", 89777, 123, 5);
+            ContaCorrente conta_da_Barbara = new ContaCorrente ("Bárbara", 89777, 965, 100);
+
+            Console.WriteLine("A conta é do(a): " + conta_do_Fabio.Titular + ", A agência é: " + conta_do_Fabio.Agencia +
+            ", O número da conta é: " + conta_do_Fabio.Numero + ", O saldo da conta é: " + conta_do_Fabio.Saldo);   
+
+            Console.WriteLine("A conta é do(a): " + conta_do_Renatin.Titular + ", A agência é: " + conta_do_Renatin.Agencia +
+            ", O número da conta é: " + conta_do_Renatin.Numero + ", O saldo da conta é: " + conta_do_Renatin.Saldo);
+
+            Console.WriteLine("A conta é do(a): " + conta_da_Barbara.Titular + ", A agência é: " + conta_da_Barbara.Agencia +
+            ", O número da conta é: " + conta_da_Barbara.Numero + ", O saldo da conta é: " + conta_da_Barbara.Saldo);
+        }
+    }
+}
